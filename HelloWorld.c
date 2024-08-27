afa51e3c842af053ba334ccc94bcc67a721acf84
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-08-27 09:56:39 -0400

    Create HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,17 @@
+// The following program will just simply print "Hello World" to the console.
+
+// Program: Lab1 - HelloWorld.c
+// Purpose: To compute the calculations of addition, subtraction, division and multiplication (for 5a).
+// Purpose: To compute the area and circumference of a circle (for 5b).
+//
+// Author: Julia Piascik
+// Course CS-241-B C PROGRAMMING CONCEPTS 
+// Term Fall 2024
+// Date: 8/27/2024
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
