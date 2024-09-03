6c78e6c24fcbd44fa5f3a2c50805bbd21be5bf26
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-09-03 10:09:38 -0400

    Update GoPioneers.c

--- GoPioneers.c
+++ GoPioneers.c
@@ -1,8 +1,8 @@
 // Program: GoPioneers.c
 // Purpose: This program is a greeting program. It demonstrates some of the components of a simple C program.
 // Author: Julia Piascik
-// Course CS 241-B C Programming Concepts
-// Term Fall 2024
+// Course: CS 241-B C Programming Concepts
+// Term: Fall 2024
 // Date: 09/03/2024
 //
 #include <stdio.h>
