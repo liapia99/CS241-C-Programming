3ac1207ee6bf2a9301434b8a2313c555cf100935
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-09-03 10:09:21 -0400

    Create HelloCProgram.c

---
+++ HelloCProgram.c
@@ -0,0 +1,18 @@
+/* Program: HelloCProgram.c 
+    Purpose: This program is a greeting program. 
+    It demonstrates some of the components of a simple C program. 
+    Author: Julia Piascik
+    Course: CS 241-B C Programming Concepts 
+    Term: Fall 2024 
+    Date: 09/03/2024 
+*/ 
+
+#include <stdio.h>
+
+/* main function */
+int main(void)
+{
+printf("Hello C Program!\n");
+return 0;
+}
+/* EOF */
