56a95407754521ed5b4bf166f18d0002507ca35b
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-09-12 10:29:20 -0400

    Update CE3b.c

--- CE3b.c
+++ CE3b.c
@@ -10,37 +10,42 @@
 #include <stdio.h>
 
 /* main function */
-
-int main(void){
-    
-/* variable définitions */
+int main(void) {
+    /* Variable definitions */
     int a, b, c, d, n;
-    double p, q, r, x;
+    double p, q, x;
+    int r;  /* 'r' should be int for the remainder */
+
+    /* Input section */
+    printf("Please enter the value for real x: ");
+    scanf("%lf", &x);  /* Use %lf for double */
     
-    printf("Please enter the value for real x:");
-    scanf("%f", &x);
-    printf("Please enter the value for integer a: "); 
-    scanf ("%d", &a);
-    printf ("Please enter the value for integer b: "); 
-    scanf ("%d", &b);
-    printf ("Please enter the value for integer c: "); 
-    scanf ("%d", &c);
-    printf ("Please enter the value for integer d: "); 
-    scanf ("%d", &d);
-    printf ("Please enter the value for integer n: "); 
-    scanf ("%d", &n);
+    printf("Please enter the value for integer a: ");
+    scanf("%d", &a);
     
-    /* computation section */
-    p = 2.0 * x * x * - 3.0 * x + 5.0;
-    q = ((double) (a + b)) / ((double) (c + d)) ;
-    r = (1<<n) % 7;
-    /* output section */
-    printf ("The value for the polynomial p is %f.\n", p);
-    printf ("The value for the quotient q is %f. \n", q);
-    printf ("The value for the remainder I is %f.\n", r);
+    printf("Please enter the value for integer b: ");
+    scanf("%d", &b);
     
-/* exit */ 
+    printf("Please enter the value for integer c: ");  /* Use lowercase 'c' */
+    scanf("%d", &c);
     
+    printf("Please enter the value for integer d: ");
+    scanf("%d", &d);
+    
+    printf("Please enter the value for integer n: ");
+    scanf("%d", &n);
+
+    /* Computation section */
+    p = 2.0 * x * x - 3.0 * x + 5.0;
+    q = ((double) (a + b)) / ((double) (c + d));
+    r = (1 << n) % 7;  /* Bitwise left shift and modulo */
+
+    /* Output section */
+    printf("The value for the polynomial p is %f.\n", p);
+    printf("The value for the quotient q is %f.\n", q);
+    printf("The value for the remainder r is %d.\n", r);  /* Print remainder as int */
+
+    /* Exit */
     return 0;
     
 } 
