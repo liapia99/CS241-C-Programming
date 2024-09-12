a62447273ce7cfd3581e59a5aa26a3f3fdba6253
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-09-12 10:27:12 -0400

    Create CE3b.c

---
+++ CE3b.c
@@ -0,0 +1,35 @@
+/* Arithmetic.c: arithmetic expresions */
+/* */
+#include <stdio.h>
+/* main function */
+int main(void){
+/* variable définitions */
+    int a, b, c, d, n;
+    double p, q, r, x;
+    
+    printf("Please enter the value for real x:");
+    scanf("%f", &x);
+    printf("Please enter the value for integer a: "); 
+    scanf ("%d", &a);
+    printf ("Please enter the value for integer b: "); 
+    scanf ("%d", &b);
+    printf ("Please enter the value for integer C: "); 
+    scanf ("%d", &c);
+    printf ("Please enter the value for integer d: "); 
+    scanf ("%d", &d);
+    printf ("Please enter the value for integer n: "); 
+    scanf ("%d", &n);
+    
+    /* computation section */
+    p = 2.0 * x * x * - 3.0 * x + 5.0;
+    q = ((double) (a + b)) / ((double) (c + d)) ;
+    r = (1<<n) % 7;
+    /* output section */
+    printf ("The value for the polynomial p is %f.\n", p);
+    printf ("The value for the quotient q is %f. \n", q);
+    printf ("The value for the remainder I is %f.\n", r);
+    
+/* exit */ 
+    return 0;
+} /* end of main */
+/* EOF */
