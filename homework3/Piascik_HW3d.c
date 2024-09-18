b4329cfcda569acc134f28007891f9b50ca288b4
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-09-17 21:11:27 -0400

    Create Piascik_HW3d.c

---
+++ homework3/Piascik_HW3d.c
@@ -0,0 +1,32 @@
+// This program is designed to convert days into years, weeks, and days. 
+// The following code is written and compiled with CLion
+//
+// Program: Piascik_HW3d.c
+// Purpose: To convert days into years, weeks, and days. 
+
+// Author: Julia Piascik
+// Course CS-241-B C PROGRAMMING CONCEPTS 
+// Term Fall 2024
+// Date: 9/17/2024
+
+#include <stdio.h>
+
+int days, years, weeks;
+
+int main() {
+
+  printf("Enter the number of days: ");
+  scanf("%d", &days);
+
+  years = days / 365; 
+  weeks = (days % 365) / 7; 
+  days = days - ((years * 365) + ( weeks * 7));
+
+  printf("Years: %d", years);
+  printf("\n");
+  printf("Weeks: %d", weeks);
+  printf("\n");
+  printf("Days: %d", days);
+
+  return 0;
+}
