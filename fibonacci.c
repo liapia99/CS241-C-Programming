fe32855d08503b953a075769c43bd32ad92276af
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-10-20 01:06:57 -0400

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int Fibonacci(int n) {
-   if(n < 2){
-    n = n;
-   }else{
-    n = Fibonacci(n - 1) + Fibonacci(n - 2);
-   }
+   if (n < 0) {
+        return -1; 
+    } else if (n == 0) {
+        return 0;  
+    } else if (n == 1) {
+        return 1;  
+    } else {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
       
 }
 
